609fccec74f2a564e389a35c5266a22401f3f397
Sakura890000 <estebanmartinez22092007@gmail.com>
2026-06-05 21:34:37 -0500

    7 escenas

--- cinematica al iniciar el juego.py
+++ cinematica al iniciar el juego.py
@@ -6,6 +6,7 @@ import turtle as tr
 import os
 from PIL import Image, ImageTk
 from PIL import ImageOps 
+import math
 
 
 ventana = tk.Tk()
@@ -35,6 +36,9 @@ balanceo_dir = 1
 espejo = False
 resta_zoom_pjs_escena5 = 0.016
 iteracion = 0
+angulo_orbita = 0.0
+angulo_propio = 0
+radio_espiral =300.0
 
 ##---frames de los personajes---##
 def obtener_frame_link(num_archivo):
@@ -616,7 +620,7 @@ def movimiento_pjs_escena6_pt3(iteracion=0):
     pj1 = ImageTk.PhotoImage(pj1_img)
     canvas.itemconfig(pj1_id, image=pj1) #actualiza la imagen del personaje 1 en el canvas
     
-    if iteracion < 10: #si el personaje 2 esta en la posicion del cofre
+    if iteracion < 7: #si el personaje 2 esta en la posicion del cofre
         #///pj2//
         canvas.coords(pj2_id, 305, 535) #obtiene las coordenadas actuales del personaje 2
         sprites_frisk = [10, 7, 4]
@@ -639,12 +643,18 @@ def movimiento_pjs_escena6_pt3(iteracion=0):
 
 def movimiento_pjs_escena6_pt4(iteracion=0):
     global pj1_id, pj1, pj2_id, pj2, pj1_img, pj2_img, frame_inicial_link, frame_inicial_frisk
+    global relay_id, relay_img, relay
     if iteracion < 10:
         canvas.coords(pj2_id, 370, 750) #obtiene las coordenadas actuales del personaje 1
         pj2_img = Image.open("Frisk.png") #importa imagen
         pj2_img = pj2_img.resize((1700, 1700))  #tamano de zelda ancho x alto
         pj2 = ImageTk.PhotoImage(pj2_img)
         canvas.itemconfig(pj2_id, image=pj2) #actualiza la imagen
+        
+        #---relay---
+        canvas.coords(relay_id, 1900, 750) #obtiene las coordenadas actuales del relay
+        relay = ImageTk.PhotoImage(relay_img)
+        canvas.itemconfig(relay_id, image=relay) #actualiza la imagen del relay en el canvas
         
         ventana.after(400, lambda: movimiento_pjs_escena6_pt4(iteracion + 1)) #llama a esta funcion cada 50 milisegundos
     else:
@@ -655,6 +665,7 @@ def movimiento_pjs_escena6_pt4(iteracion=0):
 
 def movimiento_pjs_escena6_pt5(iteracion=0):
     global pj1_id, pj1, pj2_id, pj2, pj1_img, pj2_img
+    global relay_id, relay_img, relay
 
     if iteracion < 15:
         
@@ -677,10 +688,16 @@ def movimiento_pjs_escena6_pt5(iteracion=0):
         pj1_img = pj1_img.resize((15, 15))  #tamano de link ancho x alto
         pj1 = ImageTk.PhotoImage(pj1_img)
         canvas.itemconfig(pj1_id, image=pj1) #actualiza la imagen del personaje 1 en el canvas
+        
+        #---relay---
+        canvas.coords(relay_id, 472, 430) #obtiene las coordenadas actuales del relay
+        relay = ImageTk.PhotoImage(relay_img)
+        canvas.itemconfig(relay_id, image=relay) #actualiza la imagen del relay en el canvas
         movimiento_pjs_escena6_pt6() #inicia la siguiente parte del movimiento de los personajes en la escena 6
 
 def movimiento_pjs_escena6_pt6(iteracion=0):
     global pj1_id, pj1, pj2_id, pj2, pj1_img, pj2_img
+    
 
     if iteracion < 5:
         #///pj2///
@@ -716,12 +733,145 @@ def movimiento_pjs_escena6_pt6(iteracion=0):
         canvas.update() #actualiza el canvas para mostrar el cambio de imagen
         ventana.after(2000, lambda: movimiento_pjs_escena6_pt6(iteracion + 1)) #llama a esta funcion cada 50 milisegundos
     else:
-        print("termino escena 6")
+        movimiento_pjs_escena6_pt7() #inicia la siguiente parte del movimiento de los personajes en la escena 6
    
+def movimiento_pjs_escena6_pt7(iteracion=0):
+    global pj1_id, pj1, pj2_id, pj2, pj1_img, pj2_img
+    global relay_id, relay_img, relay
+    global direccion
+
+    if iteracion < 65:
+       #relay
+        if iteracion == 0:
+            canvas.coords(relay_id, 350, 335) #obtiene las coordenadas actuales del relay
+        relay_img = Image.open("Relay1.jpg")
+        relay_img = relay_img.resize((700, 730))  #tamano de zelda ancho x alto
+        relay = ImageTk.PhotoImage(relay_img)
+        canvas.itemconfig(relay_id, image=relay) #actualiza la imagen del relay
+        canvas.move(relay_id, 0, direccion * 2) #mueve el fondo en la direccion actual
+        
+        x, y = canvas.coords(relay_id) #obtiene las coordenadas actuales del fondo
+        
+        if y >= 335:
+            direccion = -1
+        elif y <= 325:
+            direccion = 1
+            
+
+        ventana.after(30, lambda: movimiento_pjs_escena6_pt7(iteracion + 1)) #llama a esta funcion cada 50 milisegundos
+    else:
+        escena7() #inicia la escena 7
+        
+def escena7():
+    global fondo_img, fondo, fondo_id
+    global pj1_img, pj1, pj1_id 
+    global pj2_img, pj2, pj2_id
+    global direccion, balanceo_dir, zoom_factor
+    global angulo_propio, angulo_orbita, radio_espiral
+    
+    
+    canvas.update() #actualiza el canvas para mostrar el cambio de imagen
+    canvas.delete("all") #limpia el canvas para la nueva escena
+    
+    # Cargar la imagen de fondo
+    fondo_img = Image.open("escenario 7 espiral.jpg")
+    fondo_img = fondo_img.resize((700, 700))
+    fondo = ImageTk.PhotoImage(fondo_img)
+    fondo_id = canvas.create_image(0, 0,anchor="nw", image=fondo)
+    
+    #personaje 1
+    pj1_img = Image.open("link.png") #importa imagen
+    pj1_img = pj1_img.resize((75, 85))
+    pj1 = ImageTk.PhotoImage(pj1_img)
+    pj1_id = canvas.create_image(370, 550, anchor="center", image=pj1)
+    
+    #personaje 2
+    pj2_img = Image.open("Frisk.png") #importa imagen
+    pj2_img = pj2_img.resize((75, 85))  
+    pj2 = ImageTk.PhotoImage(pj2_img)
+    pj2_id = canvas.create_image(270, 545, anchor="center", image=pj2)
+    
+    direccion = 1.0
+    balanceo_dir =  1.0
+    zoom_factor = 1.0
+    angulo_orbita = 0.0
+    angulo_propio = 0
+    radio_espiral = 300.0
+    
+    mov_escena7()
+    
+def mov_escena7(iteracion = 0):
+    global pj1, pj1_img, pj1_id
+    global pj2, pj2_img, pj2_id
+    global angulo_orbita, angulo_propio, radio_espiral
+    
+    if not canvas.winfo_exists():
+        return
+    
+    if iteracion <  100 and radio_espiral > 5:
+        #centro de la img
+        centro_x = 350
+        centro_y = 345
+        
+        #gira en circulos
+        angulo_orbita += 0.15
+        #acerca el centro
+        radio_espiral -= 3.0
+        
+        #posicion
+        nuevo_x1 = centro_x + (radio_espiral * math.cos(angulo_orbita))
+        nuevo_y1 = centro_y + (radio_espiral * math.sin(angulo_orbita))
+        
+        angulo_orbita2 = angulo_orbita + math.pi
+        
+        nuevo_x2 = centro_x + (radio_espiral * math.cos(angulo_orbita2))
+        nuevo_y2 = centro_y + (radio_espiral * math.sin(angulo_orbita2))
+        #movemos al nene
+        canvas.coords(pj1_id, nuevo_x1, nuevo_y1)
+        canvas.coords(pj2_id, nuevo_x2, nuevo_y2)
+        
+        #Rotacion viva la coca
+        angulo_propio =(angulo_propio + 12) % 360
+        
+        pj1_img = Image.open("link.png")
+        pj2_img = Image.open("Frisk.png")
+        
+        #escala
+        escala = radio_espiral / 300.0 
+        if escala < 0.1: 
+            escala = 0.1 #limite de size
+        
+        w, h = int(75 * escala), int (85 * escala)
+        img_petit1 = pj1_img.resize((w, h))
+        img_petit2 = pj2_img.resize((w, h))
+        
+        #no fumen
+        img_rotada1 = img_petit1.rotate(angulo_propio, expand= True)
+        img_rotada2 = img_petit2.rotate(angulo_propio, expand= True)
+        
+        #updateo el canvas para q tk lo lea
+        pj1 = ImageTk.PhotoImage(img_rotada1)
+        canvas.itemconfig(pj1_id, image=pj1)
+        pj2 = ImageTk.PhotoImage(img_rotada2)
+        canvas.itemconfig(pj2_id, image=pj2)
+        
+        
+        #RECURSIVIDAD PAQ VEA Q SI SE NOJOA
+        ventana.after(40, lambda: mov_escena7(iteracion + 1))
+    else: 
+        print("hoakakamg")
+        
+        
+    
+    
+                                 
+        
+
+
     
     
 
-escena6() #inicia la sexta escena
+escena7() #inicia la sexta escena
 
 
 ventana.mainloop()
